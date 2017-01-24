9f0be3a142477a2029f430f1071491ae511ab84f
Eric Mellino <erme@microsoft.com>
2017-01-24 15:30:09 -0800

    Initial commit of NativeLibraryLoader

---
+++ NativeLibraryLoader/LibraryLoader.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace NativeLibraryLoader
+{
+    /// <summary>
+    /// Exposes functionality for loading native libraries and function pointers.
+    /// </summary>
+    public abstract class LibraryLoader
+    {
+        /// <summary>
+        /// Loads a native library by name and returns an operating system handle to it.
+        /// </summary>
+        /// <param name="name">The name of the library to open.</param>
+        /// <returns>The operating system handle for the shared library.</returns>
+        public IntPtr LoadNativeLibrary(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Parameter must not be null or empty.", nameof(name));
+            }
+
+            return CoreLoadNativeLibrary(name);
+        }
+
+        /// <summary>
+        /// Loads a function pointer out of the given library by name.
+        /// </summary>
+        /// <param name="handle">The operating system handle of the opened shared library.</param>
+        /// <param name="functionName">The name of the exported function to load.</param>
+        /// <returns>A pointer to the loaded function.</returns>
+        public IntPtr LoadFunctionPointer(IntPtr handle, string functionName)
+        {
+            if (string.IsNullOrEmpty(functionName))
+            {
+                throw new ArgumentException("Parameter must not be null or empty.", nameof(functionName));
+            }
+
+            return CoreLoadFunctionPointer(handle, functionName);
+        }
+
+        /// <summary>
+        /// Frees the library represented by the given operating system handle.
+        /// </summary>
+        /// <param name="handle">The handle of the open shared library.</param>
+        public void FreeNativeLibrary(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new ArgumentException("Parameter must not be zero.", nameof(handle));
+            }
+
+            CoreFreeNativeLibrary(handle);
+        }
+
+        /// <summary>
+        /// Loads a native library by name and returns an operating system handle to it.
+        /// </summary>
+        /// <param name="name">The name of the library to open. This parameter must not be null or empty.</param>
+        /// <returns>The operating system handle for the shared library.</returns>
+        protected abstract IntPtr CoreLoadNativeLibrary(string name);
+
+        /// <summary>
+        /// Frees the library represented by the given operating system handle.
+        /// </summary>
+        /// <param name="handle">The handle of the open shared library. This must not be zero.</param>
+        protected abstract void CoreFreeNativeLibrary(IntPtr handle);
+
+        /// <summary>
+        /// Loads a function pointer out of the given library by name.
+        /// </summary>
+        /// <param name="handle">The operating system handle of the opened shared library. This must not be zero.</param>
+        /// <param name="functionName">The name of the exported function to load. This must not be null or empty.</param>
+        /// <returns>A pointer to the loaded function.</returns>
+        protected abstract IntPtr CoreLoadFunctionPointer(IntPtr handle, string functionName);
+
+        /// <summary>
+        /// Returns a default library loader for the running operating system.
+        /// </summary>
+        /// <returns>A LibraryLoader suitable for loading libraries.</returns>
+        public static LibraryLoader GetPlatformDefaultLoader()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return new Win32LibraryLoader();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                return new UnixLibraryLoader();
+            }
+
+            throw new PlatformNotSupportedException("This platform cannot load native libraries.");
+        }
+
+        private class Win32LibraryLoader : LibraryLoader
+        {
+            protected override void CoreFreeNativeLibrary(IntPtr handle)
+            {
+                Kernel32.FreeLibrary(handle);
+            }
+
+            protected override IntPtr CoreLoadFunctionPointer(IntPtr handle, string functionName)
+            {
+                return Kernel32.GetProcAddress(handle, functionName);
+            }
+
+            protected override IntPtr CoreLoadNativeLibrary(string name)
+            {
+                return Kernel32.LoadLibrary(name);
+            }
+        }
+
+        private class UnixLibraryLoader : LibraryLoader
+        {
+            protected override void CoreFreeNativeLibrary(IntPtr handle)
+            {
+                Libdl.dlclose(handle);
+            }
+
+            protected override IntPtr CoreLoadFunctionPointer(IntPtr handle, string functionName)
+            {
+                return Libdl.dlsym(handle, functionName);
+            }
+
+            protected override IntPtr CoreLoadNativeLibrary(string name)
+            {
+                Libdl.dlerror();
+                IntPtr handle = Libdl.dlopen(name, Libdl.RTLD_NOW);
+                if (handle == IntPtr.Zero && !Path.IsPathRooted(name))
+                {
+                    string localPath = Path.Combine(AppContext.BaseDirectory, name);
+                    handle = Libdl.dlopen(localPath, Libdl.RTLD_NOW);
+                }
+
+                return handle;
+            }
+        }
+    }
+}
---
+++ NativeLibraryLoader/NativeLibrary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NativeLibraryLoader
+{
+    /// <summary>
+    /// Represents a native shared library opened by the operating system.
+    /// This type can be used to load native function pointers by name.
+    /// </summary>
+    public class NativeLibrary : IDisposable
+    {
+        private static readonly LibraryLoader s_platformDefaultLoader = LibraryLoader.GetPlatformDefaultLoader();
+        private readonly LibraryLoader _loader;
+
+        /// <summary>
+        /// The operating system handle of the loaded library.
+        /// </summary>
+        public IntPtr Handle { get; }
+
+        /// <summary>
+        /// Constructs a new NativeLibrary using the platform's default library loader.
+        /// </summary>
+        /// <param name="name">The name of the library to load.</param>
+        public NativeLibrary(string name) : this(name, s_platformDefaultLoader)
+        {
+        }
+
+        /// <summary>
+        /// Constructs a new NativeLibrary using the specified library loader.
+        /// </summary>
+        /// <param name="name">The name of the library to load.</param>
+        /// <param name="loader">The loader used to open and close the library, and to load function pointers.</param>
+        public NativeLibrary(string name, LibraryLoader loader)
+        {
+            _loader = loader;
+            Handle = _loader.LoadNativeLibrary(name);
+        }
+
+        /// <summary>
+        /// Loads a function whose signature matches the given delegate type's signature.
+        /// </summary>
+        /// <typeparam name="T">The type of delegate to return.</typeparam>
+        /// <param name="name">The name of the native export.</param>
+        /// <returns>A delegate wrapping the native function.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when no function with the given name
+        /// is exported from the native library.</exception>
+        public T LoadFunction<T>(string name)
+        {
+            IntPtr functionPtr = _loader.LoadFunctionPointer(Handle, name);
+            if (functionPtr == IntPtr.Zero)
+            {
+                throw new InvalidOperationException($"No function was found with the name {name}.");
+            }
+
+            return Marshal.GetDelegateForFunctionPointer<T>(functionPtr);
+        }
+
+        /// <summary>
+        /// Frees the native library. Function pointers retrieved from this library will be void.
+        /// </summary>
+        public void Dispose()
+        {
+            _loader.FreeNativeLibrary(Handle);
+        }
+    }
+}
